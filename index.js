2e75a21bc70091896a4e1e556c800e2da1c86e46
Jule Engel <jule.engel1@iu-study.org>
2023-11-22 16:48:54 +0100

    Kommentare eingebaut

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-//Anzeige verschiedener Alternativen je nach Auswahl
+/*
+    Student: Jule Engel
+    Modul: IPWA01-01 - Programmierung von Webanwendungen
+    Aufgabe: Fallstudie, Aufgabe 3
+*/
+
+
+//Anzeige verschiedener Alternativen je nach Auswahl Abholung / Abgabe.
+// Wird jedes Mal aktiviert, wenn zwischen Abholung und Abgabe gewechselt wird.
+//Aufgabe 3.b.g
 function anzeigeSammeldienst() {
     let sammeldienstChecked = document.getElementById('sammeldienst').checked;
     let sammeldienstAktiv = document.getElementById('sammeldienstAktiv');
@@ -10,6 +19,8 @@ function anzeigeSammeldienst() {
     let ort = document.getElementById('ort');
     let phone = document.getElementById(`phone`);
 
+    //Bei Abholung durch Sammeldienst stelle Adress-Einstellungen auf sichtbar.
+    //Die Telefon-Nummer wird bei einer Abholung für organisatorische Zwecke angefordert.
     if (sammeldienstChecked) {
       sammeldienstAktiv.style.display = 'block';
       abgabeAktiv.style.display = `none`;
@@ -18,8 +29,9 @@ function anzeigeSammeldienst() {
       plz.required = true;
       ort.required = true;
       phone.required = true;
-    } else {
-      //Setze Input-Felder für die Adressen zurück, da diese nun nicht mehr benötigt werden
+    } 
+    //Setze Input-Felder für die Adressen zurück, da diese nun nicht mehr benötigt werden; modifiziere Sichtbarkeit.
+    else {
       sammeldienstAktiv.style.display = 'none';
       abgabeAktiv.style.display = `block`;
       strasse.required = false;
@@ -28,6 +40,7 @@ function anzeigeSammeldienst() {
       nummer.value = "";
       plz.required = false;
       plz.value = "";
+      //Da die Plz individuell validiert wird, muss der Wert hier zurückgesetzt werden.
       plz.setCustomValidity('');
       ort.required = false;
       ort.value = "";
@@ -35,11 +48,14 @@ function anzeigeSammeldienst() {
     }
 }
 
+
+//Überprüfung der Postleitzahl; 
+//Aufgabe 3.b.h
 function validatePLZ(value, element) {
-    // Überprüfen, ob die PLZ in 20--- Hamburg liegt
+    //Überprüfen, ob die PLZ in 20--- Hamburg liegt
     let validPLZ = /^20\d{3}$/.test(value);
 
-    // Anzeige der Fehlermeldung je nach Validierungsergebnis
+    //Anzeige der Fehlermeldung je nach Validierungsergebnis
     if (validPLZ) {
         element.setCustomValidity('');
     } else {
@@ -47,41 +63,43 @@ function validatePLZ(value, element) {
     }
 }
 
-
+//Die Funktion überprüft, ob mindestens eine Art von Kleidung für die Spende ausgewählt wurde.
 function validateCheckboxes() {
-    // Hole alle Checkboxen mit dem Namen "selectedCheckboxes"
+    //Hole alle Checkboxen mit dem Namen "selectedCheckboxes".
     const checkboxes = document.querySelectorAll('input[name="kleiderCheckboxes"]');
       
-    // Überprüfe, ob mindestens eine Checkbox ausgewählt ist
+    //Überprüfe, ob mindestens eine Checkbox ausgewählt ist.
     const atLeastOneSelected = Array.from(checkboxes).some(checkbox => checkbox.checked);
 
-    // Zeige eine Meldung, wenn keine Checkbox ausgewählt ist
+    //Zeige eine Meldung, wenn keine Checkbox ausgewählt ist -> Validierung fehlgeschlagen.
     if (!atLeastOneSelected) {
       checkboxes.forEach(checkbox => {
         checkbox.setCustomValidity("Bitte wähle mindestens ein Kleidungsstück aus!");
       })
     } else {
-      // Hier könntest du das Formular absenden oder weitere Aktionen durchführen
+      //Validierung erfolgreich.
       checkboxes.forEach(checkbox => {
         checkbox.setCustomValidity("");
       })
     }
 }
 
-// Funktion zum Zurücksetzen des Formulars
+
+// Funktion zum Zurücksetzen des Formulars -> wird benötigt, um die Anzeigeeinstellungen für Abholung / Abgabe zurückzusetzen.
 function resetForm() {
     document.getElementById('registrierungsFormular').reset();
+    //Setzt Anzeige zurück und leert die Felder.
     anzeigeSammeldienst();
   }
-
+//Verhindert Anzeige-Fehler beim Neuladen des Fensters.
 window.onload = anzeigeSammeldienst();
 
-// Bootstrap Validierung der Daten
+
+//Bootstrap Validierung der Daten.
 (() => {
     'use strict'
     const forms = document.querySelectorAll('.needs-validation')
   
-    // Loop over them and prevent submission
     Array.from(forms).forEach(form => {
       form.addEventListener('submit', event => {
         if (!form.checkValidity()) {
@@ -95,53 +113,69 @@ window.onload = anzeigeSammeldienst();
   })()
 
 
-//Hinzufügen Eingaben zum lokalen Speicher
+//Hinzufügen der Eingaben zum lokalen Speicher, um sie nach erfolgreicher Registrierung anzeigen zu lassen.
+//Event-Listener reagiert auf Submit-Button.
+//Aufgabe 3.b.i
 document.getElementById(`registrierungsFormular`).addEventListener(`submit`, function(event) {
+    //Vor der Speicherung der Angaben wird überprüft, ob alle Eingaben Valide sind. Bei Fehler wird eine Meldung ausgegeben.
     validateCheckboxes(event);
     if(!document.getElementById(`registrierungsFormular`).checkValidity()) {
       document.getElementById(`registrierungsFormular`).reportValidity();
     }
+    //Speichern der Eingaben im Local Storage.
     else {
-      //Speichern der Eingaben im Local Storage
       const registrierungsFormular = document.getElementById(`registrierungsFormular`);
       const formData = {};
       const kleiderCheckboxesList = {};
 
       for (const element of registrierungsFormular.elements) {
-        // Element der Gruppe kleiderCheckboxes
+        //Elemente der Gruppe kleiderCheckboxes für die Angabe der Kleidungsart.
         if (element.name == `kleiderCheckboxes`) {
+          //Es werden alle Elemente mitsamt der Boolean-Werte in einem extra Object gespeichert.
           kleiderCheckboxesList[element.value] = element.checked;
         }
+        //Elemente von "radio"-Eingaben.
         else if (element.type == `radio`) {
+          //Es wird nur das aktive Element gespeichert.
           if (element.checked) {
             formData[element.name] = element.value;
           }
         }
+        //Speicherung der restlichen Elemente.
         else if (element.name) {
-          // Wenn es eine Checkbox ist, speichere einen booleschen Wert abhängig vom checked-Zustand
+          //Wenn es eine Checkbox ist, speichere einen booleschen Wert abhängig vom checked-Zustand.
           formData[element.name] = (element.type === 'checkbox') ? element.checked : element.value;
         }
       }
+      //Die Liste mit der Auswahl der Kleidung wird nun unter dem key "kleider" gespeichert.
       formData[`kleider`] = kleiderCheckboxesList;
 
+      //Speicherung Datum und Uhrzeit der Registrierung.
       let datum = new Date();
+      //Formatierung zur besseren Lesbarkeit.
       let registrierDatum = datum.toLocaleDateString('de-DE', { year: 'numeric', month: '2-digit', day: '2-digit' });
       let registrierZeit = formatZeroes(datum.getHours()) + ":" + formatZeroes(datum.getMinutes()) + ":" + formatZeroes(datum.getSeconds());
       formData[`registrierZeit`] = registrierZeit;
       formData[`registrierDatum`] = registrierDatum;
 
+      //Wenn ein Ort in der Registrierung angegeben wurde, wird er hier gespeichert.
       let registrierOrt = "";
       if (formData[`ort`] != "") {
         registrierOrt = formData[`ort`];
       }
       formData[`registrierOrt`] = registrierOrt;
 
+      //Speichere die Datei `formData` mit den Nutzer-Eingaben im lokalen Speicher.
       localStorage.setItem('formData', JSON.stringify(formData));
-      console.log('Formulardaten im Local Storage:', formData);
+      //Verhindere das ursprüngliche Verhalten, um ein reines Neu-Laden der Seite mit den Eingabe-Feldern in der Adressleiste zu verhindern.
+      //Dies ist notwendig, da keine explizite Methode der Datenverarbeitung genannt wurde und somit standardmäßig "Pre" genommen wird.
+      //Post wäre hierbei sicherer, benötigt allerdings serverseitige Anbindungen, die nicht Teil der Aufgabenstellung sind.
+      //Mehr Informationen in der Fallstudie.
       event.preventDefault();
+      //Weiterleitung nach erfolgreicher Registrierung
       window.location.href = "./registriert.html"
 
-      //Anpassung Darstellung Zeit
+      //Anpassung Darstellung Zeit für einstellige Werte.
       function formatZeroes(time) {
         time = time.toString();
         return time.length < 2 ? "0" + time : time;
--- registriert.js
+++ registriert.js
@@ -1,20 +1,31 @@
+/*
+    Student: Jule Engel
+    Modul: IPWA01-01 - Programmierung von Webanwendungen
+    Aufgabe: Fallstudie, Aufgabe 3
+*/
 
+//Zeige Daten nach erfolgreicher Registrierung auf extra Seite.
+//Aufgabe 3.b.i
 function registriertAnzeigeAbgeschlossen() {
-    // Überprüfen, ob formData im Local Storage vorhanden ist
+    //Überprüfen, ob formData im Local Storage vorhanden ist.
     if (localStorage.getItem('formData')) {
-        // formData abrufen und in einer Variable speichern
+        //formData abrufen und in einer Variable speichern.
         const formData = JSON.parse(localStorage.getItem('formData'));
+
+        //Elemente aus dem HTML verändern, sodass die gespeicherten Daten angezeigt werden.
         let titel = formData[`title`];
-        titel = titel.toUpperCase().charAt(0) + titel.slice(1)
+        titel = titel.toUpperCase().charAt(0) + titel.slice(1) //Verbesserte Anzeige, Großbuchstabe am Anfang.
+        //Für alle Anzeigen wird innerText verwendet -> siehe Fallstudie.
         document.getElementById(`registriertName`).innerText = "Name: " + titel + " " + formData[`vorname`] + " " + formData[`nachname`];
         document.getElementById('registriertEmail').innerText = "E-Mail: " + formData[`email`];
         
+        //Nur Anzeige, wenn Telefon-Nummer angegeben wurde.
         if (formData[`phone`] != "") {
             document.getElementById(`registriertPhone`).innerText = "Telefon-Nummer: " + formData[`phone`];
         }
 
         if (formData[`sammeldienstAbfrage`] == `sammeldienst`) {
-            //Sicherheitsvorkehrung -> Validierung müsste leere Felder hier abfangen
+            //Sicherheitsvorkehrung -> Validierung müsste leere Felder hier abfangen.
             if (formData[`strasse`] != "" && formData[`hausnummer`] != "" && formData[`plz`] != "" && formData[`ort`] != "") {
                 document.getElementById(`registriertAdresse`).innerText = 
                     "Ihre Adresse: " + 
@@ -37,17 +48,20 @@ function registriertAnzeigeAbgeschlossen() {
             document.getElementById(`registriertAnzahlPakete`).innerText = formData[`anzahl`] + " Pakete / Tüten mit folgender Kleidung:";
         }
 
+        //Objekt für Auswahl der Kleider wird ausgewählt und auf einer Konstanten gespeichert. Eine zusätzliche Variable wird für die letztendliche Ausgabe erzeugt.
         const kleider = formData[`kleider`];
         let kleiderAktiv = "";
 
+        //Jedes Kleidungsstück wird durchgegangen -> Nur ausgewählte Kleidung wird in die Anzeige übernommen.
         Object.entries(kleider).forEach(([key, value]) => {
             if (kleider[key]) {
                 kleiderAktiv += key.toUpperCase().charAt(0) + key.slice(1) + ", ";
             }
         });
-        //Entferne letztes , 
+        //Entferne letztes ", " aus der Anzeige.
         kleiderAktiv = kleiderAktiv.slice(0, -2);
         document.getElementById(`registriertKleidung`).innerText = kleiderAktiv;
+
 
         if (formData[`krisengebiet`] == "unbestimmt") {
             document.getElementById(`registriertKrisengebiet`).innerText = "Sie haben uns die Auswahl des Krisengebietes überlassen!"
@@ -58,6 +72,7 @@ function registriertAnzeigeAbgeschlossen() {
             document.getElementById(`registriertKrisengebiet`).innerText = "Das Krisengebiet für Ihre Spende ist: " + krisengebiet;
         }
 
+        //Jeweilige Anzeigen mit Informationen je nach Abholung oder Abgabe.
         if (formData[`sammeldienstAbfrage`] == `sammeldienst`) {
             document.getElementById(`abgabeMethode`).innerText = "Abgabe-Methode: Sammeldienst";
             document.getElementById(`registriertAbgabe`).innerText = "Es wird sich bald jemand telefonisch bei Ihnen melden, um die weitere Organisation zu besprechen!"
@@ -67,6 +82,7 @@ function registriertAnzeigeAbgeschlossen() {
             document.getElementById(`registriertAbgabe`).innerText = "Unsere Abgabestelle befindet sich in der Musterstraße 123, 20095 Hamburg. Die Öffnungszeiten sind Mo.- Sa. 10-17 Uhr. Vor Ort können Sie mithilfe Ihrer E-Mail-Adresse Ihre Angaben aufrufen."
         }
 
+        //Anzeige von Ort, Datum und Zeit.
         if (formData[`registrierOrt`] == "") {
             document.getElementById(`registriertDatum`).innerText = formData[`registrierDatum`] + " um " + formData[`registrierZeit`] + " Uhr";
         }
@@ -74,11 +90,13 @@ function registriertAnzeigeAbgeschlossen() {
             document.getElementById(`registriertDatum`).innerText = formData[`registrierOrt`] +  ", der " + formData[`registrierDatum`] + " um " + formData[`registrierZeit`] + " Uhr";
         }
 
-        console.log(formData);
-
-    } else {
-        console.log('Es gibt keine formData im Local Storage.');
+    } 
+    //Wenn keine Daten im Lokalen Speicher sind, erscheint eine Fehlermeldung für den Nutzer.
+    else {
+        document.getElementById(`registrierungErfolgreich`).style.display = "none";
+        document.getElementById(`registrierungFehlgeschlagen`).style.display = "block"
     }
 }
 
+//Sobald das Fenster geladen wird, werden die Daten eingeblendet.
 window.onload = registriertAnzeigeAbgeschlossen();
